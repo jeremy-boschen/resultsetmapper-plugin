f515b89bb1c019772ccb76ca774cf8d33b95d35c
GitHub Action <action@github.com>
2024-05-17 13:28:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/jeremyboschen/resultsetmapperplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/jeremyboschen/resultsetmapperplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.jeremyboschen.resultsetmapperplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jeremyboschen.resultsetmapperplugin.MyBundle
+import com.github.jeremyboschen.resultsetmapperplugin.services.MyProjectService
 import javax.swing.JButton
 
 
