848caff76fbdb36a0a90b83be41fa8a1d570f2dc
Jeremy Boschen <newtycoffee@gmail.com>
2024-05-29 12:24:20 -0400

    Fix type mapping for List,Set,Collection fields

--- src/test/testData/fromRowMethod/after_FromRowMethodCreatedOnDTO.java
+++ src/test/testData/fromRowMethod/after_FromRowMethodCreatedOnDTO.java
@@ -3,9 +3,15 @@ package com.jb.test;
 import com.jb.jdbc.ResultSetWrapper;
 
 public class MyDTO {
+    public static MyDTO2 {
+        private String valueOne;
+    }
+
     private String columnOne;
     private Integer columnTwo;
     private long columnThree;
+    private List<Long> columnFor;
+    private Set<MyDTO2> columnFive;
 
     @javax.annotation.Generated("ResultSetMapperIntention")
     public static MyDTO fromRow(ResultSetWrapper rsw) throws java.sql.SQLException {
@@ -13,6 +19,8 @@ public class MyDTO {
         e.columnOne = rsw.getString("columnone");
         e.columnTwo = rsw.getInt("columntwo");
         e.columnThree = rsw.getLong("columnthree", e.columnThree);
+        //TODO: No ResultSetWrapper mapping exists for columnFor:List<Long>
+        //TODO: No ResultSetWrapper mapping exists for columnFive:Set<MyDTO2>
         return e;
     }
 }
